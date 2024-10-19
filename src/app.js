385c420c1358b5be65cd0497f9a2061ed01a408f
Juan Acevedo <juanfdoa@hotmail.com>
2024-10-19 11:15:01 -0500

    change package

--- src/app.js
+++ src/app.js
@@ -18,6 +18,11 @@ app.use('/api/organizations', organizationsRoutes);
 app.set('PORT', process.env.PORT || 3000);
 
 // Iniciar el servidor
-app.listen(app.get('PORT'), () => {
-  console.log(`Servidor escuchando en el puerto`, app.get('PORT'));
+//app.listen(app.get('PORT'), () => {
+//  console.log(`Servidor escuchando en el puerto`, app.get('PORT'));
+//});
+
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
